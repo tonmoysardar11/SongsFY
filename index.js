ebefbf5620028a4fd5b85ddd546c572778f8b800
Tonmoy Sardar <110713788+tonmoysardar11@users.noreply.github.com>
2023-01-03 20:33:43 +0530

    added playable timestamp

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-console.log("SongsFY")
+// console.log("SongsFY")
 // variables
 let songIndex = 0;
-let audioElem = new Audio('Warsaw.mp3');
+let audioElem = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let songList = Array.from(document.getElementsByClassName('songItem'));
 // audio list array
 let songs = [
     { songName: 'Blank Space', filePath: '1.mp3', coverPath: '1.jpg' },
-    { songName: 'Gangsta`s Paradise', filePath: '2.mp3', coverPath: '2.jfif' },
+    { songName: 'Gangsta`s Paradise', filePath: '2.mp3', coverPath: '2.jpg' },
     { songName: 'Hall of Fame', filePath: '3.mp3', coverPath: '3.jpg' },
     { songName: 'Immortals', filePath: '4.mp3', coverPath: '4.jpg' },
     { songName: 'Let me love you', filePath: '5.mp3', coverPath: '5.jpg' },
@@ -24,7 +24,7 @@ songList.forEach((element, i) => {
     // console.log(songs[i].coverPath)
     element.getElementsByClassName('albumArt')[0].src = songs[i].coverPath;
     element.getElementsByClassName('songName')[0].innerText = songs[i].songName;
-    // element.getElementsByClassName('timeStamp')[0].innerText=songs[i].duration;
+
 });
 
 // play/pause
@@ -52,6 +52,32 @@ audioElem.addEventListener('timeupdate', () => {
     // console.log('timeupdate')
     progress = ((audioElem.currentTime / audioElem.duration) * 100);
     myProgressBar.value = progress;
+    var seconds = audioElem.currentTime % 60;
+    var foo = audioElem.currentTime - seconds;
+    var fixedSeconds= seconds.toFixed(2);
+    var minutes = foo / 60;
+    if (fixedSeconds < 10) {
+        fixedSeconds = "0" + fixedSeconds.toString();
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes.toString();
+    }
+    var fixedCurrentTime = minutes + ":" + fixedSeconds;
+
+    var seconds2 = audioElem.duration % 60;
+    var aaa = audioElem.duration - seconds2;
+    var minutes2 = aaa / 60;
+    var fixedSeconds2= seconds2.toFixed(2);
+    if (fixedSeconds2 < 10) {
+        fixedSeconds2 = "0" + fixedSeconds2.toString();
+    }
+    if (minutes2 < 10) {
+        minutes2 = "0" + minutes2.toString();
+    }
+    var fixedDuration = minutes2 + ":" + fixedSeconds2;
+
+    document.getElementById('ct').innerText = fixedCurrentTime;
+    document.getElementById('dur').innerText = fixedDuration;
 });
 
 myProgressBar.addEventListener('change', () => {
@@ -61,9 +87,14 @@ myProgressBar.addEventListener('change', () => {
 
 const songPlay = (element) => {
     Array.from(document.getElementsByClassName('playSong')).forEach((element) => {
-    element.classList.remove('fa-pause')
-    element.classList.add('fa-play')
-    document.getElementById('gif').style.opacity = 0;
+        element.classList.remove('fa-pause')
+        element.classList.add('fa-play')
+        document.getElementById('gif').style.opacity = 0;
+    })
+}
+const timeStamp = (element) => {
+    Array.from(document.getElementsByClassName('timeStamp')).forEach((element) => {
+        console.log(element.inner)
     })
 }
 
@@ -71,28 +102,26 @@ Array.from(document.getElementsByClassName('playSong')).forEach((element) => {
     element.addEventListener('click', (e) => {
         // console.log(e.target)
         songPlay();
-        songIndex= parseInt(e.target.id)
+        songIndex = parseInt(e.target.id)
         e.target.classList.remove('fa-play');
         e.target.classList.add('fa-pause');
-        audioElem.src=`${songIndex}.mp3`
+        audioElem.src = `${songIndex}.mp3`
         audioElem.play();
         audioElem.currentTime = 0;
         masterPlay.classList.remove('fa-play');
         masterPlay.classList.add('fa-pause');
         document.getElementById('gif').style.opacity = 1;
-        document.getElementById('masterSong').innerText = songs[songIndex-1].songName;
+        document.getElementById('masterSong').innerText = songs[songIndex - 1].songName;
     })
 })
-document.getElementById('next').addEventListener('click',()=>{
-    if(songIndex>9)
-    {
-        songIndex=0;
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex > 9) {
+        songIndex = 0;
     }
-    else
-    {
+    else {
         songIndex += 1;
-        audioElem.src=`${songIndex}.mp3`
-        document.getElementById('masterSong').innerText = songs[songIndex-1].songName;
+        audioElem.src = `${songIndex}.mp3`
+        document.getElementById('masterSong').innerText = songs[songIndex - 1].songName;
         audioElem.play();
         audioElem.currentTime = 0;
         masterPlay.classList.remove('fa-play');
@@ -101,16 +130,14 @@ document.getElementById('next').addEventListener('click',()=>{
     }
 })
 
-document.getElementById('back').addEventListener('click',()=>{
-    if(songIndex<=0)
-    {
-        songIndex=0;
+document.getElementById('back').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 0;
     }
-    else
-    {
+    else {
         songIndex -= 1;
-        audioElem.src=`${songIndex}.mp3`
-        document.getElementById('masterSong').innerText = songs[songIndex-1].songName;
+        audioElem.src = `${songIndex}.mp3`
+        document.getElementById('masterSong').innerText = songs[songIndex - 1].songName;
         audioElem.play();
         audioElem.currentTime = 0;
         masterPlay.classList.remove('fa-play');
